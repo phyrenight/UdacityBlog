16eb0d08b39caca77700b7bd0448e1a719820703
Marc <phyrenight@gmail.com>
2016-05-29 01:36:41 -0400

    add error messaging to blogform

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import os
+
 import jinja2
 import webapp2
 
+# import models
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                  autoescape = True)
@@ -18,11 +20,29 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
+    def render_Html(self, title="", rant="", error=""):
+        self.render("blogform.html", title=title, rant=rant, error=error)
+    
     def get(self):
-        title = self.request.get_all("title")
-        self.render("blogform.html", title=title)
+        self.render_Html()
 
-app = webapp2.WSGIApplication([('/blog', MainPage),],
+    def post(self):
+    	title = self.request.get("title")
+    	rant = self.request.get("rant")
+        
+    	if not title or not rant:
+            error = True
+            self.render_Html(title, rant, error) 
+            
+    	else:
+            self.redirect("/blogpost")
+
+
+class BlogPage(Handler):
+    def get(self):
+        self.render('blogpost.html')
+
+app = webapp2.WSGIApplication([('/blog', MainPage),('/blogpost',BlogPage)],
                                debug=True)
 
 
